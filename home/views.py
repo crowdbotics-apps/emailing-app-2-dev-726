adeb95b49e141d039315882806596be4a55c7320
Crowdbotics <team@crowdbotics.com>
2019-04-05 18:51:41 +0000

    remove package djangorestframework

--- home/views.py
+++ home/views.py
@@ -7,7 +7,6 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
-	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
